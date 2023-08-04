40534ced5e252de872b385b92b8eea1bc54c73ad
unknown <dmanankichian@gmail.com>
2023-08-03 21:52:04 -0700

    enemy ships generate randomly. not overlapping or going off the board

--- app.js
+++ app.js
@@ -1,9 +1,100 @@
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
 const optionContainer = document.querySelector('.option-container')
 const flipButton = document.querySelector('#flip-button')
 
+// Option Choosing
+let angle = 0
 function flip () {
    const optionShips = Array.from(optionContainer.children)
-   optionShips.forEach(optionShip => optionShip.style.transform = `rotate(90deg)`)
+
+   angle = angle === 0 ? 90 : 0
+
+   optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
 }
- 
-flipButton.addEventListener('click', flip)
+
+
+// Creating Boards
+const width = 10
+function createBoard(color, user) {
+    const gameBoardContainer = document.createElement('div')
+    gameBoardContainer.classList.add('game-board')
+    gameBoardContainer.style.backgroundColor = color
+    gameBoardContainer.id = user
+
+    for (let i = 0; i < width * width; i++){
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
+    }
+
+    gamesBoardContainer.append(gameBoardContainer)
+}
+
+createBoard('gray', 'player')
+createBoard('darkred', 'computer')
+
+// Creating Ships
+class Ship {
+    constructor(name, length) {
+        this.name = name
+        this.length = length
+    }
+}
+
+const destroyer = new Ship('destroyer', 2)
+const submarine = new Ship('submarine', 3)
+const cruiser = new Ship('cruiser', 3)
+const battleship = new Ship('battleship', 4)
+const carrier = new Ship('carrier', 5)
+
+const ships = [destroyer, submarine, cruiser, battleship, carrier]
+
+function addShipPiece(ships) {
+    const allBoardBlocks = document.querySelectorAll('#computer div')
+    let randomBoolean = Math.random() < 0.5
+    let isHorizontal = randomBoolean
+    let randomStartIndex = Math.floor(Math.random() * width * width)
+    
+    let validStart = isHorizontal ? randomStartIndex <= width * width - ships.length ? randomStartIndex :
+        width * width - ships.length :
+    // handle verticle
+    randomStartIndex <= width * width - width * ships.length ? randomStartIndex : 
+        randomStartIndex - ships.length * width + width
+        console.log(validStart)
+    let shipBlocks = []
+    for (let i = 0; i < ships.length; i++) {
+        if (isHorizontal) {
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i])
+            console.log("if statement", shipBlocks)
+        } else {
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i * width])
+            console.log("else statement", shipBlocks)
+        }
+    }
+
+    let valid
+
+    if (isHorizontal) {
+    shipBlocks.every((_shipBlock, index) => 
+        valid = shipBlocks[0].id % width !== width - (shipBlocks.length - (index + 1)))
+    } else {
+        shipBlocks.every((_shipBlock, index) => 
+            valid = shipBlocks[0].id < 90 + (width * index + 1)
+        )
+    }
+
+    const notTaken = shipBlocks.every(shipBlock => !shipBlock.classList.contains('taken'))
+
+    if (valid && notTaken) {
+        shipBlocks.forEach((shipBlock, index) => {
+        shipBlock.classList.add(ships.name)
+        shipBlock.classList.add('taken')
+        
+    })
+    } else {
+        addShipPiece(ships)
+    }
+
+ }
+  ships.forEach(ship => addShipPiece(ship))
